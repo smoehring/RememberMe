6840cc9c32a6b21b6ce95cd2f9ccf2097ccb6c72
kytecat <87901430+kytecat@users.noreply.github.com>
2024-05-24 22:57:47 +0200

    Adding Idle-Timer to store Notifications if User is AFK

--- RememberMe.Ui.WPF/Services/ContactMonitorService.cs
+++ RememberMe.Ui.WPF/Services/ContactMonitorService.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Runtime.InteropServices;
+using Microsoft.Extensions.Logging;
+using Snoval.Dev.RememberMe.Ui.WPF.Models;
 
 namespace Snoval.Dev.RememberMe.Ui.WPF.Services;
 
@@ -6,6 +8,8 @@ public class ContactMonitorService(ILogger<ContactMonitorService> logger, Notifi
 {
     private PeriodicTimer? _timer;
     private List<Guid> _contactsNotified = new();
+    private List<ContactEntry> _delayedNotifications = new();
+    private const int IdleTimeMinutes = 5;
 
     public async Task ExecuteAsync(CancellationToken stoppingToken)
     {
@@ -38,17 +42,73 @@ public class ContactMonitorService(ILogger<ContactMonitorService> logger, Notifi
             var newExpiredContacts = configDataContext.Config.Contacts.Where(c => !_contactsNotified.Contains(c.Uuid) 
                 && c.TimespanUuid != Guid.Empty 
                 && c.LastContact.AddSeconds(configDataContext.Config.Timespans.First(timespan => timespan.Uuid.Equals(c.TimespanUuid)).Timespan) < DateTime.Now).ToList();
+
+            if (_delayedNotifications.Count > 0)
+            {
+                newExpiredContacts.AddRange(_delayedNotifications);
+            }
+            
             if (newExpiredContacts.Count != 0)
             {
                 foreach (var contact in newExpiredContacts)
                 {
-                    notificationManager.SendDueNotification(contact);
-                    logger.LogDebug("Adding {contact} to the list of notified contacts", contact.Uuid);
+                    // Check if User is even around, otherwise store the notification for later
+                    var idleTime = TimeSpan.FromSeconds(GetLastInputTime());
+                    
+                    if (idleTime > TimeSpan.FromMinutes(IdleTimeMinutes))
+                    {
+                        if (!_delayedNotifications.Contains(contact))
+                        {
+                            logger.LogDebug("User is not around, storing notification for later");
+                            _delayedNotifications.Add(contact);
+                        }
+                        continue;
+                    }
+
                     _contactsNotified.Add(contact.Uuid);
+                    notificationManager.SendDueNotification(contact);
+                    if (_delayedNotifications.Contains(contact))
+                    {
+                        _delayedNotifications.RemoveAll(entry => entry.Uuid == contact.Uuid);
+                    }
+                    logger.LogDebug("Adding {contact} to the list of notified contacts", contact.Uuid);
+
                 }
             }
             
             
         } while (await _timer.WaitForNextTickAsync(stoppingToken));
     }
+
+    #region GlobalUserActivity
+
+    [DllImport("user32.dll")]
+    static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct LASTINPUTINFO
+    {
+        public static readonly int SizeOf = Marshal.SizeOf(typeof(LASTINPUTINFO));
+        [MarshalAs(UnmanagedType.U4)] public int cbSize;
+        [MarshalAs(UnmanagedType.U4)] public uint dwTime;
+    }
+
+    static uint GetLastInputTime()
+    {
+        uint idleTime = 0;
+        LASTINPUTINFO lastInputInfo = new();
+        lastInputInfo.cbSize = Marshal.SizeOf(lastInputInfo);
+        lastInputInfo.dwTime = 0;
+        
+        var envTicks = (uint) Environment.TickCount;
+        if (GetLastInputInfo(ref lastInputInfo))
+        {
+            var lastInputTick = lastInputInfo.dwTime;
+            idleTime = envTicks - lastInputTick;
+        }
+        return ((idleTime > 0) ? (idleTime / 1000) : 0);
+    }
+
+    #endregion
+    
 }
